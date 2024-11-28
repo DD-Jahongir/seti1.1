25557e8ded3d17f69e8da927cb7cd57734acf78e
Jahongir <138986850+DD-Jahongir@users.noreply.github.com>
2024-11-28 21:14:16 +0300

    Add files via upload

---
+++ server1.1.cpp
@@ -0,0 +1,53 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+struct Person {
+    char name[25]; // имя
+    int height;    // рост
+    int weight;    // вес
+} B;
+
+int answer;
+long size_pred;
+
+int main() {
+    ifstream fR;
+    ofstream fA;
+    setlocale(LC_ALL, "rus");
+    const char* nameR = "C:\\Users\\пк\\OneDrive\\Документы\\KubSU\\comp nets\\Infile.txt";
+    const char* nameA = "C:\\Users\\пк\\OneDrive\\Документы\\KubSU\\comp nets\\Outfile.txt";
+
+    cout << "Сервер работает..." << endl;
+
+    fR.open(nameR, ios::binary);
+    fR.seekg(0, ios::end);
+    size_pred = fR.tellg(); // Запоминаем начальную позицию в файле запросов
+    fR.close();
+
+    while (true) {
+        fR.open(nameR, ios::binary);
+        fR.seekg(0, ios::end);
+
+        while (size_pred >= fR.tellg()) {
+            Sleep(100);
+            fR.seekg(0, ios::end);
+        }
+
+        fR.seekg(size_pred, ios::beg);
+        fR.read((char*)&B, sizeof(B));
+        size_pred = fR.tellg();
+        fR.close();
+
+        double IMT = (double)B.weight / ((0.01 * B.height) * (0.01 * B.height));
+        if (IMT < 18.5) answer = 0;
+        else if (IMT < 25) answer = 1;
+        else answer = 2;
+
+        fA.open(nameA, ios::binary | ios::app);
+        fA.write((char*)&IMT, sizeof(IMT)); // Записываем IMT
+        fA.write((char*)&answer, sizeof(answer)); // Записываем категорию
+        fA.close();
+    }
+}
---
+++ user1.1.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <fstream>
+#include <windows.h>
+using namespace std;
+
+struct Person {
+    char name[25];
+    int height;
+    int weight;
+} A;
+
+int main() {
+    setlocale(LC_ALL, "rus");
+    const char* nameR = "C:\\Users\\пк\\OneDrive\\Документы\\KubSU\\comp nets\\Infile.txt";
+    const char* nameA = "C:\\Users\\пк\\OneDrive\\Документы\\KubSU\\comp nets\\Outfile.txt";
+    ofstream f_req;
+    ifstream f_ans;
+    long pred_size;
+    int answer;
+    double IMT;
+
+    while (true) {
+        cout << "Запрос: фамилия, рост (в см), вес (в кг): " << endl;
+        cin >> A.name >> A.height >> A.weight;
+        f_req.open(nameR, ios::app | ios::binary);
+        f_req.write((char*)&A, sizeof(A));
+        f_req.close();
+
+        f_ans.open(nameA, ios::binary);
+        f_ans.seekg(0, ios::end);
+        pred_size = f_ans.tellg();
+        while (pred_size >= f_ans.tellg()) {
+            Sleep(100);
+            f_ans.seekg(0, ios::end);
+        }
+        f_ans.seekg(pred_size, ios::beg);
+        f_ans.read((char*)&IMT, sizeof(IMT)); // Чтение значения IMT
+        f_ans.read((char*)&answer, sizeof(answer)); // Чтение категории
+
+        f_ans.close();
+
+        cout << "Ваш ИМТ: " << IMT << endl;
+        switch (answer) {
+        case 0: cout << "Недостаток веса\n"; break;
+        case 1: cout << "Норма\n"; break;
+        case 2: cout << "Избыточный вес\n"; break;
+        }
+    }
+}
